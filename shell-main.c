1e6cd3c522662f9d78d948faf0befcda81c63d0e
KM8Oz <ouazmir@gmail.com>
2023-05-22 10:40:36 -0700

    Simple shell 0.1 +

--- shell-main.c
+++ shell-main.c
@@ -1,34 +1,76 @@
-#include "utils/main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * run - constract comand from buffer and execute it,
+ * @pid: ...
+ * @buffer: ...
+ * Return: void.
+ */
+
+void run(pid_t pid, char buffer[BUFFER_SIZE])
+{
+	int i, status;
+	char *args[64];
+
+	if (pid == 0)
+	{
+		args[0] = strtok(buffer, " ");
+		for (i = 1; i < 64; i++)
+		{
+			args[i] = strtok(NULL, " ");
+			if (args[i] == NULL)
+				break;
+		}
+		execve(args[0], args, NULL);
+		perror("execve");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(&status);
+	}
+}
+
+/**
+ * main - simple shell
+ * Return: int.
+ */
 
 int main(void)
 {
-char line[1024];
-int input;
-int first;
-char *cmd;
-char *next;
+	char buffer[BUFFER_SIZE];
+	size_t len;
+	pid_t pid;
 
-printf("SIMPLE SHELL: Type 'exit' or send EOF to exit.\n");
-while (1)
-{
-printf("$> ");
-fflush(NULL);
-if (!fgets(line, sizeof(line), stdin))
-return (0);
-input = 0;
-first = 1;
-cmd = line;
-next = strchr(cmd, '|');
-while (next != NULL)
-{
-*next = '\0';
-input = run(cmd, input, first, 0);
-cmd = next + 1;
-next = strchr(cmd, '|');
-first = 0;
-}
-input = run(cmd, input, first, 1);
-}
+	while (1)
+	{
+		printf("($) ");
+		fflush(stdout);
+		if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
+			break;
+		len = strlen(buffer);
+		if (len > 0 && buffer[len - 1] == '\n')
+			buffer[len - 1] = '\0';
+		if (strcmp(buffer, "exit") == 0)
+			break;
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			run(pid, buffer);
+		}
+	}
 
-return (0);
+	return (EXIT_SUCCESS);
 }
